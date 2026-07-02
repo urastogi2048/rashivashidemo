ef238a3e9af6d63f3bbe852b174c6025f587d312
urastogi2048 <utkarshrastogi2048@gmail.com>
2026-07-03 00:09:23 +0530

    feat:ideal-motion

--- rashivashi_app/lib/widgets/tarotcard.dart
+++ rashivashi_app/lib/widgets/tarotcard.dart
@@ -3,6 +3,7 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:rashivashi_app/widgets/tarot_back.dart';
 import 'package:rashivashi_app/widgets/tarot_front.dart';
+
 class TarotCard extends StatefulWidget {
   const TarotCard({super.key});
   @override
@@ -10,7 +11,7 @@ class TarotCard extends StatefulWidget {
 }
 
 class _TarotCardState extends State<TarotCard> with TickerProviderStateMixin {
-  bool flipped=true;
+  bool flipped = true;
   late AnimationController flipcontroller;
   late AnimationController glowcontroller;
   late Animation<double> rotationAnimation;
@@ -18,70 +19,103 @@ class _TarotCardState extends State<TarotCard> with TickerProviderStateMixin {
   late Animation<double> glowBlur;
   late Animation<double> glowSpread;
   late final glowCurve;
-  @override 
+  late AnimationController idleController;
+  late Animation<double> idleOffset;
+  @override
   void initState() {
     super.initState();
-    flipped=false;
-    flipcontroller=AnimationController(vsync: this,duration: const Duration(milliseconds: 800), reverseDuration: const Duration(milliseconds: 500));
-    glowcontroller=AnimationController(vsync: this,duration: const Duration(milliseconds: 500));
-    rotationAnimation=CurvedAnimation(parent: flipcontroller, curve: Curves.easeInOut);
-    curve=Tween<double>(begin: 0.0, end: math.pi).animate(rotationAnimation);
-    glowCurve=CurvedAnimation(parent: glowcontroller, curve: Curves.easeInOut);
-    glowBlur=Tween<double>(begin: 0.0, end: 35.0).animate(glowCurve);
-    glowSpread=Tween<double>(begin: 0.0, end: 8.0).animate(glowCurve);
+    flipped = false;
+    flipcontroller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+      reverseDuration: const Duration(milliseconds: 500),
+    );
+    glowcontroller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    rotationAnimation = CurvedAnimation(
+      parent: flipcontroller,
+      curve: Curves.easeInOut,
+    );
+    curve = Tween<double>(begin: 0.0, end: math.pi).animate(rotationAnimation);
+    glowCurve = CurvedAnimation(
+      parent: glowcontroller,
+      curve: Curves.easeInOut,
+    );
+    glowBlur = Tween<double>(begin: 0.0, end: 35.0).animate(glowCurve);
+    glowSpread = Tween<double>(begin: 0.0, end: 8.0).animate(glowCurve);
+
+    idleController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat(reverse: true);
+    idleOffset = Tween<double>(
+      begin: -10.0,
+      end: 10.0,
+    ).animate(CurvedAnimation(parent: idleController, curve: Curves.easeInOut));
   }
+
   @override
-  void dispose(){
+  void dispose() {
     flipcontroller.dispose();
     glowcontroller.dispose();
+    idleController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () async {
-        if(!flipped){
+        if (!flipped) {
           await flipcontroller.forward();
           await glowcontroller.forward();
           //await glowcontroller.reverse();
-        }else{
+        } else {
           glowcontroller.reverse();
           flipcontroller.reverse();
         }
-        flipped=!flipped;
-       
+        flipped = !flipped;
       },
       child: AnimatedBuilder(
-        animation: Listenable.merge([flipcontroller, glowcontroller]),
-        
+        animation: Listenable.merge([flipcontroller, glowcontroller, idleController]),
+
         builder: (context, child) {
-          final angle =curve.value;
+          final angle = curve.value;
           print(angle);
-          return 
-          Transform(alignment: Alignment.center, 
-          transform: Matrix4.identity()..setEntry(3, 2, 0.002)
-                                       ..rotateY(angle),  
-          child: angle <= math.pi / 2 ? TarotBack() : Container(
-              decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0xFFFFD54F).withOpacity(0.8),
-                  blurRadius: glowBlur.value,
-                  spreadRadius: glowSpread.value,
-                ),
-                ],
-              ),
-            child: Transform(alignment: Alignment.center,transform: Matrix4.identity()
-                                         ..rotateY(math.pi), child: TarotFront()),
-          ) );
+          return Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.002)
+              ..rotateY(angle),
+            child: angle <= math.pi / 2
+                ? Transform.translate(
+                    offset: Offset(
+                      0,
+                      angle <= math.pi / 2 ? idleOffset.value : 0,
+                    ),
+                    child: TarotBack(),
+                  )
+                : Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xFFFFD54F).withOpacity(0.8),
+                          blurRadius: glowBlur.value,
+                          spreadRadius: glowSpread.value,
+                        ),
+                      ],
+                    ),
+                    child: Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()..rotateY(math.pi),
+                      child: TarotFront(),
+                    ),
+                  ),
+          );
         },
       ),
-
-         
-     
-        
-      
-    
     );
   }
 }
